5f4e6941760e789f7d31bc31410878833c1edaf0
sumedhbhaskar <sumedh.bhaskar@gmail.com>
2019-06-17 23:48:48 +0530

    models updated, dummy views created and connected with urls

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hello world")
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at the question %s" % question_id )
+
+def results(request, question_id):
+    return HttpResponse("you are looking at the result of question %s" % questiom_id)
+
+def vote(request, question_id):
+    return HttpResponse("you are votig on question %s" % question_id) 
