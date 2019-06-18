68d9814bb478b22fdca02e4848c7bb7767b62841
sumedhbhaskar <sumedh.bhaskar@gmail.com>
2019-06-19 00:14:24 +0530

    results.html created... views,admin modified

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
-from django.http import HttpResponse
+from .models import Question, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -11,7 +12,18 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse("you are looking at the result of question %s" % questiom_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
+
+
 
 def vote(request, question_id):
-    return HttpResponse("you are votig on question %s" % question_id) 
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+            return render(request, 'polls/detail.html',{'question':question, 'error_message': "you didnt select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args= (question.id,)))
